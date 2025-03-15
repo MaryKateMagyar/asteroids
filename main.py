1cede6fd772b31ca117f2a39cad06f595e972eed
MaryKateMagyar <marykatemagyar@gmail.com>
2025-03-15 11:49:46 -0700

    fix venv and add clock to main.py

--- main.py
+++ main.py
@@ -3,16 +3,22 @@ import constants as c
 
 def main():
     pygame.init()
+
     print("Starting Asteroids!")
     print(f"Screen width: {c.SCREEN_WIDTH}")
     print(f"Screen height: {c.SCREEN_HEIGHT}")
+
     screen = pygame.display.set_mode((c.SCREEN_WIDTH, c.SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
