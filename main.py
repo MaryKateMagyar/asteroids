45f227aafdf55a195918a091c85f47ed21ef6745
MaryKateMagyar <marykatemagyar@gmail.com>
2025-03-15 11:36:51 -0700

    update .gitignore to remove venv, create constants.py, create game loop and window starting point in main.py

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import pygame
+import constants as c
 
 def main():
+    pygame.init()
     print("Starting Asteroids!")
+    print(f"Screen width: {c.SCREEN_WIDTH}")
+    print(f"Screen height: {c.SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((c.SCREEN_WIDTH, c.SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
