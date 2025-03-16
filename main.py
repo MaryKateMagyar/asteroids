459d6e0f7fe9967d5edf111c5e93ec1d6838395b
MaryKateMagyar <marykatemagyar@gmail.com>
2025-03-15 19:33:35 -0700

    create player.py, update main.py and contants.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import constants as c
+import player as p
 
 def main():
     pygame.init()
@@ -9,6 +10,7 @@ def main():
     print(f"Screen height: {c.SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((c.SCREEN_WIDTH, c.SCREEN_HEIGHT))
+    player_character = p.Player((c.SCREEN_WIDTH / 2), (c.SCREEN_HEIGHT / 2))
     clock = pygame.time.Clock()
     dt = 0
 
@@ -17,6 +19,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player_character.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
