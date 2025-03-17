90abbf06b3be8811eff9bf361c4aadbb7b69f1da
MaryKateMagyar <marykatemagyar@gmail.com>
2025-03-17 11:46:13 -0700

    add error handling

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pygame
 import constants as c
 import game_systems as gs
 
+
 def main():
     print("Starting Asteroids!")
 
@@ -10,13 +11,23 @@ def main():
     player_character, asteroid_field = gs.setup_game_objects(updatable_group, drawable_group, asteroids_group, shots_group)
     dt = 0
 
-    while True:
-        gs.handle_events()
-        gs.render_screen(screen, drawable_group)
-        gs.update_game_state(updatable_group, asteroids_group, shots_group, player_character, dt)
-        pygame.display.flip()
-        dt = clock.tick(cs.FRAME_RATE) / 1000
+    try:
+        while True:
+            gs.handle_events()
+            gs.render_screen(screen, drawable_group)
+            gs.update_game_state(updatable_group, asteroids_group, shots_group, player_character, dt)
+            pygame.display.flip()
+            dt = clock.tick(c.FRAME_RATE) / 1000
 
+    except Exception as e:
+        print("An error occurred during the game loop!")
+        print(f"Error details: {e}")
+
+    finally:
+        print("Exiting game. Cleaning up resources.")
+        pygame.quit()
+        sys.exit()
+    
 
 if __name__ == "__main__":
     main()
